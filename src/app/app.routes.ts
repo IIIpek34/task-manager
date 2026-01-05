629225eb5cf76145c3d86310af32c4957e3fb8db
IIIpek34 <bekeshevdimasik@gmail.com>
2026-01-06 01:41:11 +0300

    feat: add routing with list and editor pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { List } from './pages/list/list';
+import { Editor } from './pages/editor/editor';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'list', component: List },
+  { path: 'editor', component: Editor },
+  { path: '**', redirectTo: 'list' }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
+
+// bootstrapApplication(App, appConfig)
+//   .catch((err) => console.error(err));
+
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule)
+  ]
+});
